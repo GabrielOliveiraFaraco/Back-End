5cab3532f029adf132d65ed182d6af149533d27f
GabrielOliveiraFaraco <gabriel.oli.faraco@gmail.com>
2025-08-14 21:29:18 -0300

    if, else if, do, for, while e realização da TDE03

---
+++ EstruturasDeRepeticao/Program.cs
@@ -0,0 +1,99 @@
+﻿/* // if else
+
+int idade = 19;
+
+if (idade >= 18)
+{
+    Console.WriteLine("Você é maior de idade.");
+}
+else
+{
+    Console.WriteLine("Você é menor de idade.");
+}
+
+// if else if
+
+double nota = 8.5;
+
+if (nota >= 9)
+{
+    Console.WriteLine("Tu é o bixão mesmo!");
+}
+else if (nota >= 6)
+{
+    Console.WriteLine("Tu é bom, mas pode melhorar.");
+}
+else
+{
+    Console.WriteLine("Tu precisa estudar mais.");
+}
+
+// Switch-case
+
+string cor = "vermelho";
+
+switch (cor)
+{
+    case "vermelho":
+        Console.WriteLine("A cor é vermelho!");
+        break;
+
+    case "verde":
+        Console.WriteLine("A cor é verde!");
+        break;
+
+    case "azul":
+        Console.WriteLine("A cor é azul!");
+        break;
+    default:
+        Console.WriteLine("Cor desconhecida!");
+        break;
+}
+
+// for loop
+
+for (int i = 0; i < 10; i += 2)
+{
+    Console.WriteLine($"Contagem: {i}");
+}
+
+// while loop
+
+int contador = 0;
+while (contador < 5)
+{
+    Console.WriteLine($"Contador: {contador}");
+    contador++;
+}
+ */
+// do while loop
+
+/* int contagem = 0;
+
+do
+{
+    Console.WriteLine($"Contagem: {contagem}");
+    contagem++;
+} while (contagem > 3);
+ */
+// break
+
+/* for (int i = 0; i < 10; i++)
+{
+    if (i == 5)
+    {
+        break;
+    }
+    Console.WriteLine($"Número: {i}");
+} */
+
+// continue
+
+for (int i = 0; i < 10; i++)
+{
+    if (i % 2 != 0)
+    {
+        continue;
+    }
+    Console.WriteLine(i);
+}
---
+++ TDE03/Program.cs
@@ -0,0 +1,42 @@
+﻿double saldo = 1000.0;
+int opcao = 0;
+
+do
+{
+    Console.WriteLine("Bem vindo ao nosso sistema!");
+    Console.WriteLine("Escolha uma das opções abaixo:");
+    Console.WriteLine("Digite 1 para consultar o saldo");
+    Console.WriteLine("Digite 2 para sacar");
+    Console.WriteLine("Digite 3 para depositar");
+    Console.WriteLine("Digite 4 para sair");
+    opcao = Convert.ToInt32(Console.ReadLine());
+
+    switch (opcao)
+    {
+        case 1:
+            Console.WriteLine($"Seu saldo é de: R$ {saldo}");
+            break;
+        case 2:
+            Console.WriteLine("Digite o valor que deseja sacar:");
+            double valorSaque = Convert.ToInt32(Console.ReadLine());
+            if (valorSaque > saldo)
+            {
+                Console.WriteLine("Saldo insuficiente para saque.");
+            }
+            else
+            {
+                saldo -= valorSaque;
+                Console.WriteLine($"Saque de R$ {valorSaque} realizado com sucesso. Novo saldo: R$ {saldo}");
+            }
+            break;
+        case 3:
+            Console.WriteLine("Digite o valor que deseja depositar:");
+            double valorDeposito = Convert.ToInt32(Console.ReadLine());
+            saldo += valorDeposito;
+            Console.WriteLine($"Depósito de R$ {valorDeposito} realizado com sucesso. Novo saldo: R$ {saldo}");
+            break;
+        case 4:
+            Console.WriteLine("Obrigado por usar nosso sistema. Até logo!");
+            break;
+    }
+}while (opcao != 4);
